2039721ef14600ec201e3584f584496347afd30b
Aakriti Bhusal <aakriti2061@gmail.com>
2024-05-24 14:12:34 +0100

    Doing tax calculator

---
+++ src/aakriti/IncomeTax.java
@@ -0,0 +1,19 @@
+package aakriti;
+
+import java.util.Scanner;
+
+public class IncomeTax {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+    int income=sc.nextInt();
+    if(income<=500000){
+        System.out.println("You don't need to pay any tax");
+    }
+    else if(income>500000 && income<1000000){
+        System.out.println("You need to pay 20% tax");
+        }
+    else{
+        System.out.println("You need to pay 30% tax");
+    }
+    }
+}
