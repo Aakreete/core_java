d64cc185c42ce43ca92c799ebc87cbf828573916
Aakriti Bhusal <aakriti2061@gmail.com>
2024-05-25 19:47:00 +0100

    Implementing while loop in Java

---
+++ src/aakriti/Loop.java
@@ -0,0 +1,11 @@
+package aakriti;
+
+public class Loop {
+    public static void main(String[] args) {
+        int counter=0;
+        while(counter<10){
+            System.out.println("Hello World");
+            counter ++;
+        }
+    }
+}
