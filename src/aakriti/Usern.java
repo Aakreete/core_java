6e8573c58b30af2330b6e33f87ba8a0d24dab607
Aakriti Bhusal <aakriti2061@gmail.com>
2024-05-25 21:48:45 +0100

    while loop

---
+++ src/aakriti/Usern.java
@@ -0,0 +1,17 @@
+package aakriti;
+
+import java.util.Scanner;
+
+public class Usern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=1;
+        System.out.println("Enter any number");
+        int n=sc.nextInt();
+        while (a<=n){
+            System.out.println(a);
+            a++;
+
+        }
+    }
+}
